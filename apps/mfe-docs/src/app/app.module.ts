6502625c21440b72829a86621a47d26adada2068
Paul Mojica <paulmojicatech@gmail.com>
2022-01-27 01:54:33 -0500

    add cors to pmt-security

--- apps/mfe-docs/src/app/app.module.ts
+++ apps/mfe-docs/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { NxWelcomeComponent } from './nx-welcome.component';
     NxWelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
