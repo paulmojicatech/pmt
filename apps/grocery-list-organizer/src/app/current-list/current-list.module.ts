34dd82703c1f18b2d10ef04792a9503d4ba9d829
Paul Mojica <paulmojicatech@gmail.com>
2022-02-13 16:09:54 -0500

    add current list tab

---
+++ apps/grocery-list-organizer/src/app/current-list/current-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CurrentListComponent } from './current-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    CurrentListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CurrentListModule { }
