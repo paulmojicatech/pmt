b41dc04d7f19500805d8090d6e38a01a16a3f38c
Paul Mojica <paulmojicatech@gmail.com>
2022-02-15 06:14:42 -0500

    add exports

--- libs/grocery-list-organizer-shared-business-logic/src/index.ts
+++ libs/grocery-list-organizer-shared-business-logic/src/index.ts
@@ -1,3 +1,4 @@
+//#region Storage Exports
 export { IonicStorageService } from './lib/services/storage/ionic-storage.service';
 export {
   IonicStorageType,
@@ -5,3 +6,16 @@ export {
   AvailableGroceryItem,
   GroceryItem,
 } from './lib/services/storage/models/storage.interface';
+
+//#endregion
+
+//#region State Exports
+
+//#region Current Items
+export * from './lib/state/current-grocery-items/actions/current-grocery-items.actions';
+export * from './lib/state/current-grocery-items/effects/current-grocery-items.effects';
+export * from './lib/state/current-grocery-items/reducer/current-grocery-items.reducer';
+export * from './lib/state/current-grocery-items/index';
+//#endregion
+
+//#endregion
