bb2e2db4848bea39d736104c80849a0ebdd50b64
Paul Mojica <paulmojicatech@gmail.com>
2022-02-15 22:09:48 -0500

    add ionic storage package

--- apps/mfe-login/src/app/register/register.component.ts
+++ apps/mfe-login/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pmt-register',
+  selector: 'pmt-register-wrapper',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
