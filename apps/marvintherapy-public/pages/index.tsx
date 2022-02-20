760694112936eaa04e56bcad8ca2b30df75eed5c
Paul Mojica <paulmojicatech@gmail.com>
2022-02-20 13:39:49 -0500

    add marvintherapy-public site

---
+++ apps/marvintherapy-public/pages/index.tsx
@@ -0,0 +1,14 @@
+import styles from './index.module.scss';
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.scss file.
+   */
+  return (
+    <h1>Marvin Therapy works</h1>
+  );
+}
+
+export default Index;
