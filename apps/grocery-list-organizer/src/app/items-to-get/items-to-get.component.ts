26d373bb2904370be00fc911cbcdc6ac0245a1c0
Paul Mojica <paulmojicatech@gmail.com>
2022-03-24 23:56:48 -0400

    clear form when modal is closed

--- apps/grocery-list-organizer/src/app/items-to-get/items-to-get.component.ts
+++ apps/grocery-list-organizer/src/app/items-to-get/items-to-get.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IonModal } from '@ionic/angular';
 import {
   ItemsToGetStateService,
   ItemsToGetViewModel,
 } from '@pmt/grocery-list-organizer-business-logic-items-to-get';
 import { GroceryItem } from '@pmt/grocery-list-organizer-shared-business-logic';
-import { Observable } from 'rxjs';
+import { from, Observable, Subject, takeLast, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'pmt-items-to-get',
   templateUrl: './items-to-get.component.html',
   styleUrls: ['./items-to-get.component.scss'],
 })
-export class ItemsToGetComponent implements OnInit {
+export class ItemsToGetComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('ionModal')
+  ionModal!: IonModal;
+
   viewModel$!: Observable<ItemsToGetViewModel>;
   itemsToGetForm!: FormGroup;
+
+  private _componentDestroyed$ = new Subject<void>();
 
   constructor(
     public itemsToGetStateSvc: ItemsToGetStateService,
@@ -29,12 +41,25 @@ export class ItemsToGetComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit(): void {
+    this.ionModal.ionModalDidDismiss
+      .pipe(takeUntil(this._componentDestroyed$))
+      .subscribe(() => {
+        this.itemsToGetForm.reset();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._componentDestroyed$.next();
+  }
+
   addItem(): void {
     const itemToAdd: GroceryItem = {
       name: this.itemsToGetForm.get('itemToAdd')?.value,
       qty: this.itemsToGetForm.get('qty')?.value,
     };
     this.itemsToGetStateSvc.addItem(itemToAdd);
+    this.itemsToGetForm.reset();
   }
 
   handleAutocompleteChangeEv(updatedValue: string): void {
