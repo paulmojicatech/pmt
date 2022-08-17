438337dd2dfef90df1fb93471eb95c8f7bfcf1f2
Paul Mojica <paulmojicatech@gmail.com>
2022-08-17 01:09:33 -0400

    add qb years

--- apps/fantalytic-public/src/app/fantasy-football/functions/fantasy-football.functions.ts
+++ apps/fantalytic-public/src/app/fantasy-football/functions/fantasy-football.functions.ts
@@ -1,8 +1,10 @@
+import { qbStats2019 } from '../../../assets/stats/2019_qb';
+import { qbStats2020 } from '../../../assets/stats/2020_qb';
 import {qbStats2018} from '../../../assets/stats/2018_qb';
 
 
 export function getQbRowData(): {[key: string]: number | string}[] {
-    return qbStats2018.map(stat => {
+    const stats2018 = qbStats2018.map(stat => {
         return {
             player: stat.player.value,
             year: 2018,
@@ -11,12 +13,28 @@ export function getQbRowData(): {[key: string]: number | string}[] {
             passingYdsPerAttempt: stat.passingYdsPerAttempt.value,
             ints: stat.ints.value
         }
-    }).sort((prev, next) => {
-        if (prev.passingYds > next.passingYds) {
-            return -1;
-        }
-        return 1;
     });
+    const stats2019 = qbStats2019.map(stat => {
+        return {
+            player: stat.player.value,
+            year: 2019,
+            passingYds: stat.passingYds.value,
+            tds: stat.tds.value,
+            passingYdsPerAttempt: stat.passingYdsPerAttempt.value,
+            ints: stat.ints.value
+        }
+    });
+    const stats2020 = qbStats2020.map(stat => {
+        return {
+            player: stat.player.value,
+            year: 2020,
+            passingYds: stat.passingYds.value,
+            tds: stat.tds.value,
+            passingYdsPerAttempt: stat.passingYdsPerAttempt.value,
+            ints: stat.ints.value
+        }
+    });
+    return [...stats2018, ...stats2019, ...stats2020];
 }
 
 
