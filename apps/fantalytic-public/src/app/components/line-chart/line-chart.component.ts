63b6dd2e2c3f44072b4f01aafbf162c8138af78e
Paul Mojica <paulmojicatech@gmail.com>
2022-08-25 07:00:21 -0400

    add test for chart

--- apps/fantalytic-public/src/app/components/line-chart/line-chart.component.ts
+++ apps/fantalytic-public/src/app/components/line-chart/line-chart.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseChartDirective, NgChartsModule } from 'ng2-charts';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'pmt-line-chart',
@@ -8,9 +9,13 @@ import { BaseChartDirective, NgChartsModule } from 'ng2-charts';
   imports: [CommonModule, NgChartsModule],
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.scss'],
+  exportAs: 'pmt-line-chart'
 })
 export class LineChartComponent {
   
+  @Input()
+  lineChartConfig?: ChartConfiguration;
+
   @ViewChild(BaseChartDirective)
   myChart!: BaseChartDirective;
   
--- apps/fantalytic-public/src/app/fantasy-football/components/compare-players/compare-players.component.ts
+++ apps/fantalytic-public/src/app/fantasy-football/components/compare-players/compare-players.component.ts
@@ -2,16 +2,35 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { ChartConfiguration } from 'chart.js';
+import { LineChartComponent } from '../../../components/line-chart/line-chart.component';
 
 @Component({
   selector: 'pmt-compare-players',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, LineChartComponent],
   templateUrl: './compare-players.component.html',
   styleUrls: ['./compare-players.component.scss'],
 })
 export class ComparePlayersComponent {
   constructor(private _router: Router) {}
+
+  TEST_CONFIG: ChartConfiguration = {
+    data: {
+      datasets: [
+        {
+          label: 'My Data Point',
+          data: [
+            {
+              x: 1,
+              y: 100
+            }
+          ]
+        }
+      ]
+    },
+    type: 'line'
+  }
 
   goBack(): void {
     this._router.navigate(['fantasy-football']);
