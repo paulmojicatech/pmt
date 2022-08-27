066ca3a6683b407fd044077ed2d533997900f704
Paul Mojica <paulmojicatech@gmail.com>
2022-08-27 00:08:08 -0400

    remove modules

--- apps/fantalytic-public/src/app/app.component.ts
+++ apps/fantalytic-public/src/app/app.component.ts
@@ -1,9 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'pmt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule, 
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    RouterModule
+  ]
 })
 export class AppComponent {
   title = 'fantalytic-public';
--- apps/fantalytic-public/src/main.ts
+++ apps/fantalytic-public/src/main.ts
@@ -1,13 +1,65 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HttpClientModule } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Route, RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppComponent } from './app/app.component';
+import { fantasyFootballReducer } from './app/fantasy-football/ngrx/reducer/fantasy-football.reducer';
+import { TopicEffects } from './app/topics/ngrx/effects/topics.effects';
+import { topicsReducer } from './app/topics/ngrx/reducer/topics.reducer';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch((err) => console.error(err));
+
+const routes: Route[] = [
+  {
+    path: 'topics',
+    loadComponent: () => import('./app/topics/topics.component').then(a => a.TopicsComponent),
+    providers: [
+      importProvidersFrom(
+        StoreModule.forFeature('topics', topicsReducer),
+        EffectsModule.forFeature([TopicEffects])
+      )
+    ]
+  },
+  {
+    path: 'fantasy-football',
+    loadChildren: () => import('./app/fantasy-football/const/fantasy-football-routes.const').then(m => m.FANTASY_FOOTBALL_ROUTES),
+    providers: [
+      importProvidersFrom(
+        StoreModule.forFeature('fantasy-football', fantasyFootballReducer)
+      )
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'topics',
+    pathMatch: 'full'
+  }
+];
+
+bootstrapApplication(
+  AppComponent,
+  {
+    providers: [
+      importProvidersFrom(
+        RouterModule.forRoot(routes),
+        StoreModule.forRoot({}),
+        EffectsModule.forRoot([]),
+        StoreDevtoolsModule.instrument({}),
+        HttpClientModule,
+        BrowserAnimationsModule
+      )
+    ]
+  }
+)
