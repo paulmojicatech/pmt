255994515471cc28a9bca20af359a31076bdd761
Paul Mojica <paulmojicatech@gmail.com>
2022-11-09 00:30:55 -0500

    add header

--- apps/fantalytic-mobile/src/app/tabs/tabs.module.ts
+++ apps/fantalytic-mobile/src/app/tabs/tabs.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
+import { HeaderComponent } from '../header/header.component';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    HeaderComponent
   ],
   declarations: [TabsPage]
 })
