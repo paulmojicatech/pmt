8057642931fa2c325e8d9e6570ca581841bbf9eb
Paul Mojica <paulmojicatech@gmail.com>
2022-11-12 09:06:09 -0500

    update settings for cap and ionic

--- apps/fantalytic-mobile/capacitor.config.ts
+++ apps/fantalytic-mobile/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'fantalytic-mobile',
+  appId: 'io.fantalytica',
+  appName: 'Fantalytic',
   webDir: 'www',
   bundledWebRuntime: false
 };
