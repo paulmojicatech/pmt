a36e2a918b7814d94da6f557fb84d1c1d79184c8
Paul Mojica <paulmojicatech@gmail.com>
2022-11-12 00:39:35 -0500

    add topic cards

--- apps/fantalytic-mobile/src/app/topics/topics.component.ts
+++ apps/fantalytic-mobile/src/app/topics/topics.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable no-underscore-dangle */
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { getTopics } from './ngrx/selectors/topics.selector';
+import { filter } from 'rxjs';
+import { loadTopics } from './ngrx/actions/topics.actions';
+import { RssFeedType } from '../../../../../libs/fantalytic-shared/src/index';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'pmt-topic',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
   templateUrl: './topics.component.html',
   styleUrls: ['./topics.component.scss'],
 })
 export class TopicsComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  _store = inject(Store);
+  topics$ = this._store.select(getTopics).pipe(filter(topics => !!topics));
+
+  ngOnInit(): void {
+   this._store.dispatch(loadTopics(RssFeedType.ESPN));
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  updateRssFeed(ev: any): void {
+    const selectedIndex = ev.target.options.selectedIndex;
+    switch (selectedIndex) {
+      case 0:
+        this._store.dispatch(loadTopics(RssFeedType.ESPN));
+        break;
+      case 1:
+        this._store.dispatch(loadTopics(RssFeedType.FOOTBALLERS));
+        break;
+      case 2:
+        this._store.dispatch(loadTopics(RssFeedType.PFF));
+        break;
+      default:
+        break;
+    }
+  }
 }
