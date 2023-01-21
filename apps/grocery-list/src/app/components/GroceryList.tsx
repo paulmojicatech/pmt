8f78aff7eff87f9f35e0afaf6cb2095ad3910429
Paul Mojica <paulmojicatech@gmail.com>
2023-01-20 23:21:56 -0500

    update

--- apps/grocery-list/src/app/components/GroceryList.tsx
+++ apps/grocery-list/src/app/components/GroceryList.tsx
@@ -1,7 +1,44 @@
+import { useState } from "react"
+
 export default function GroceryList() {
+    const [items, setItems] = useState<{datePurchased: string, name: string; qty: number}[]>([]);
+    
     return (
         <main>
-            Hello World
+            <section>
+
+                <table>
+                    <thead>
+                        <th>
+                            Date Purchased
+                        </th>
+                        <th>
+                            Item
+                        </th>
+                        <th>
+                            Qty
+                        </th>
+                    </thead>
+                    <tbody>
+                        {
+                            items.map(item => (
+                                <>
+                                <td>
+                                    {item.datePurchased}
+                                </td>
+                                <td>
+                                    {item.name}
+                                </td>
+                                <td>
+                                    {item.qty}
+                                </td>
+                                </>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </section>
+            
         </main>
     )
 }
