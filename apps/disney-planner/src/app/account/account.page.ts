88533e502a0f74436e7ba5b633d1896a4b913709
Paul Mojica <paulmojicatech@gmail.com>
2023-11-25 15:44:10 -0500

    add tabs remove boilerplate

---
+++ apps/disney-planner/src/app/account/account.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'disney-account',
+  standalone: true,
+  template: `
+    <div>Account</div>
+  `,
+  styles: ``
+})
+export class AccountPage {}
