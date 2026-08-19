0b63c2b0dd05d38a45e9ffc37542b6729c1d15e8
Paul Mojica <paulmojicatech@gmail.com>
2026-08-19 08:44:12 -0400

    fix: update assembly info

--- apps/marvintherapy-api/marvintherapy-api/obj/Debug/net6.0/marvintherapy-api.AssemblyInfo.cs
+++ apps/marvintherapy-api/marvintherapy-api/obj/Debug/net6.0/marvintherapy-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("marvintherapy-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a7cd62f0c60011e6937d58670ee7de78667882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+332110c160667e17205d1de54b883119fa896254")]
 [assembly: System.Reflection.AssemblyProductAttribute("marvintherapy-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("marvintherapy-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
